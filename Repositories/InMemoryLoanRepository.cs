56b631d5cee1d7cab696029b32ca2fb13cc2f601
TauG <tauraigombera@gmail.com>
2024-03-31 12:51:09 +0200

    added InMemoryLoanRepository

---
+++ Repositories/InMemoryLoanRepository.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.CompilerServices;
+using EmployeeLoans.Api.Models;
+
+namespace EmployeeLoans.Api.Reposities;
+
+public class InMemoryLoanRepository
+{
+    private readonly List<Loan> loans =
+    [
+        new Loan 
+        {
+            Id = 1,
+            LoanAmount = 1000000,
+            LoanPurpose = "Buying a car",
+            ApplicationDate = DateTime.Now,
+            MonthlyDeduction = 100000
+        },
+        new Loan 
+        {
+            Id = 2,
+            LoanAmount = 2000000,
+            LoanPurpose = "Buying a house",
+            ApplicationDate = DateTime.Now,
+            MonthlyDeduction = 200000
+        },
+        new Loan 
+        {
+            Id = 3,
+            LoanAmount = 3000000,
+            LoanPurpose = "Buying furniture",
+            ApplicationDate = DateTime.Now,
+            MonthlyDeduction = 300000
+        }
+    ];
+
+    public IEnumerable<Loan> GetLoans()
+    {
+        return loans;
+    }
+
+    public Loan GetLoan(int id)
+    {
+        return loans.SingleOrDefault(loan => loan.Id == id)!;
+    }
+}
