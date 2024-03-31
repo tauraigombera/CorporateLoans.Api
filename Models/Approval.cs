07eeae119ba582247ac30326bb39b75fecd82b80
TauG <tauraigombera@gmail.com>
2024-03-31 09:17:03 +0200

    added Approval model

---
+++ Models/Approval.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeLoans.Api.Models;
+
+public class Approval
+{
+    public int Id { get; set; }
+    public decimal LoanAmount { get; set; }
+    public required string LoanPurpose { get; set; }
+    public decimal MonthlyDeduction { get; set; }
+    public DateTime ApplicationDate { get; set; }
+}
