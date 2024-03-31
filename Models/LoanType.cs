6377601da97a3969f5142116a714a92380419259
TauG <tauraigombera@gmail.com>
2024-03-31 09:10:40 +0200

    added LoanType Model

---
+++ Models/LoanType.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeLoans.Api.Models;
+
+public class LoanType
+{
+    public int Id { get; set; }
+    public required string LoanTypeName { get; set; }
+    public required string LoanTypeDescription { get; set; }
+    public decimal MaxLoanAmount { get; set; }
+    public decimal InterestRate { get; set; }
+    public required string Collateral { get; set; }
+    public int RepaymentPeriod { get; set; } //number of months
+}
