5ff0ff4dbd2ce6a026a03b2ff8eab4d115bc1e54
TauG <tauraigombera@gmail.com>
2024-03-31 17:10:04 +0200

    added LoanController

---
+++ Controllers/LoansController.cs
@@ -0,0 +1,37 @@
+﻿using EmployeeLoans.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeLoans.Api.Controllers;
+
+[ApiController]
+[Route("loans")]
+public class LoansController : ControllerBase
+{
+    private readonly ILoanRepository _loanRepository;
+
+    public LoansController(ILoanRepository loanRepository)
+    {
+        _loanRepository = loanRepository;
+    }
+
+    //GET /loans
+    [HttpGet]
+    public IEnumerable<Loan> GetLoans()
+    {
+        var loans = _loanRepository.GetLoans();
+        return loans;
+    }
+
+    //GET /loans/{id}
+    [HttpGet("{id}")]
+    public ActionResult<Loan> GetLoan(int id)
+    {
+        var loan = _loanRepository.GetLoan(id);
+
+        if (loan is null){
+            return NotFound();
+        }
+        return Ok(loan);
+    }
+
+}
