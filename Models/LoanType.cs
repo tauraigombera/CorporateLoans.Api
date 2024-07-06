ecd3d2c946dcb5b1dbd5163fa6a4aa6d9574d43c
TauG <tauraigombera@gmail.com>
2024-07-06 19:17:45 +0200

    Added validations

--- Models/LoanType.cs
+++ Models/LoanType.cs
@@ -1,12 +1,23 @@
-﻿namespace EmployeeLoans.Api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeLoans.Api.Models;
 
 public class LoanType
 {
     public int Id { get; set; }
+    [Required]
+    [StringLength(50)]
     public required string LoanTypeName { get; set; }
+    [Required]
+    [StringLength(100)]
     public required string LoanTypeDescription { get; set; }
+    [Required]
     public decimal MaxLoanAmount { get; set; }
+    [Required]
     public decimal InterestRate { get; set; }
+    [Required]
+    [StringLength(50)]
     public required string Collateral { get; set; }
+    [Required]
     public int RepaymentPeriod { get; set; } //number of months
 }
