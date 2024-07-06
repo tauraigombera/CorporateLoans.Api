0f4489953658bf97982bc09446e659c324d81ca4
TauG <tauraigombera@gmail.com>
2024-07-06 19:19:55 +0200

    Added validations

--- Models/Approval.cs
+++ Models/Approval.cs
@@ -1,9 +1,16 @@
-﻿namespace EmployeeLoans.Api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeLoans.Api.Models;
 
 public class Approval
 {
     public Guid Id { get; set; }
+    [Required]
+    [StringLength(50)]
     public required string ApprovalStaus { get; set; }
+    [Required]
+    [StringLength(250)]
     public required string Comment   { get; set; }
+    [Required]
     public DateTime ApprovalDate { get; set; }
 }
