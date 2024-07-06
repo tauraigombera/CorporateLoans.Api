f956716e0efcf4f7763d3ef42243e0d46d203f9a
TauG <tauraigombera@gmail.com>
2024-07-06 19:12:42 +0200

    Added validations

--- Models/Loan.cs
+++ Models/Loan.cs
@@ -1,10 +1,17 @@
-﻿namespace EmployeeLoans.Api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeLoans.Api.Models;
 
 public class Loan
 {
     public Guid Id { get; set; }
+    [Required]
     public decimal LoanAmount { get; set; }
+    [Required]
+    [StringLength(250)]
     public required string LoanPurpose { get; set; }
+    [Required]
     public DateTime ApplicationDate { get; set; }
+    [Required]
     public decimal MonthlyDeduction { get; set; }
 }
