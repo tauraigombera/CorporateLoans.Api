7dbf7413fdcad40589d87587a372c11ec59462b1
TauG <tauraigombera@gmail.com>
2024-07-07 09:21:05 +0200

    added EF LoanRepository class

---
+++ Repositories/LoansRepository.cs
@@ -0,0 +1,41 @@
+﻿using EmployeeLoans.Api.Data;
+using EmployeeLoans.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeLoans.Api.Repositories;
+
+public class LoansRepository : ILoanRepository
+{
+    private readonly ApiDbContext dbContext;
+
+    public LoansRepository(ApiDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public IEnumerable<Loan> GetLoans()
+    {
+        return dbContext.Loans.AsNoTracking().ToList();
+    }
+
+    public Loan? GetLoan(Guid id)
+    {
+        return dbContext.Loans.SingleOrDefault(loan => loan.Id == id);
+    }
+
+    public void CreateLoan(Loan loan)
+    {
+        dbContext.Loans.Add(loan);
+        dbContext.SaveChanges();
+    }
+
+    public void UpdateLoan(Loan updatedLoan){
+        dbContext.Loans.Update(updatedLoan);
+        dbContext.SaveChanges();
+    }
+
+    public void DeleteLoan(Guid id)
+    {
+        dbContext.Loans.Where(loan => loan.Id == id).ExecuteDelete();
+    }
+}
