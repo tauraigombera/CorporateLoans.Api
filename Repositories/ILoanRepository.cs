538002382fe69ff494025d12d5c9bb6950a546e3
TauG <tauraigombera@gmail.com>
2024-07-07 09:21:39 +0200

    updated the Interface

--- Repositories/ILoanRepository.cs
+++ Repositories/ILoanRepository.cs
@@ -5,6 +5,12 @@ namespace EmployeeLoans.Api.Repositories;
 public interface ILoanRepository
 {
     IEnumerable<Loan> GetLoans();
-    Loan GetLoan(Guid id);
+
+    Loan? GetLoan(Guid id);
+
     void CreateLoan(Loan loan);
+
+    void UpdateLoan(Loan updatedLoan);
+
+    void DeleteLoan(Guid id);
 }
