fe3dae902fed4b9f062b3cc6ff607fec840713e3
TauG <tauraigombera@gmail.com>
2024-07-13 11:03:34 +0200

    modified approval model

--- Models/Approval.cs
+++ Models/Approval.cs
@@ -1,16 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace EmployeeLoans.Api.Models;
-
+﻿namespace EmployeeLoans.Api.Models;
 public class Approval
 {
     public Guid Id { get; set; }
-    [Required]
-    [StringLength(50)]
     public required string ApprovalStaus { get; set; }
-    [Required]
-    [StringLength(250)]
     public required string Comment   { get; set; }
-    [Required]
     public DateTime ApprovalDate { get; set; }
 }
