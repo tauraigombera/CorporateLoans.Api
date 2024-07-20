03122d02cd50099503481f0790a79e5388837108
TauG <tauraigombera@gmail.com>
2024-07-20 21:08:30 +0200

    added ApprovalHistoryExtension

---
+++ Extensions/ApprovalHistoryExtension.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeLoans.Api.Dtos.ApprovalDtos;
+using EmployeeLoans.Api.Models;
+
+namespace EmployeeLoans.Api.Extensions;
+
+public static class ApprovalHistoryExtension
+{
+    public static ApprovalHistoryDto AsDto(this ApprovalHistory approvalHistory)
+    {
+        return new ApprovalHistoryDto
+        (
+            approvalHistory.Id,
+            approvalHistory.LoanId,
+            approvalHistory.ApprovalOffice,
+            approvalHistory.ApprovalStatus,
+            approvalHistory.Comment,
+            approvalHistory.Date
+        );
+    }
+}
