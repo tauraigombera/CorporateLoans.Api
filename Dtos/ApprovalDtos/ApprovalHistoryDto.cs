6d02f46e3ce8fa971f20fd519a45d52ae04816b2
TauG <tauraigombera@gmail.com>
2024-07-20 13:13:38 +0200

    updated Dtos

---
+++ Dtos/ApprovalDtos/ApprovalHistoryDto.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeLoans.Api.Enums;
+
+namespace EmployeeLoans.Api.Dtos.ApprovalDtos;
+
+public record Approval
+(
+    Guid Id,
+    Guid LoanId,
+    string ApprovalOffice,
+    LoanStatus LoanStatus,
+    string Comment,
+    DateTime Date
+);
