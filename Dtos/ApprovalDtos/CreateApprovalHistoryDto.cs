8dfe8511bfbafbf433761711e70f87406de8aca4
TauG <tauraigombera@gmail.com>
2024-07-20 21:07:49 +0200

    added CreateApprovalHistoryDto

---
+++ Dtos/ApprovalDtos/CreateApprovalHistoryDto.cs
@@ -0,0 +1,12 @@
+﻿using EmployeeLoans.Api.Enums;
+
+namespace EmployeeLoans.Api.Dtos.ApprovalDtos;
+
+public record CreateApprovalHistoryDto
+(
+    Guid LoanId,
+    string ApprovalOffice,
+    ApprovalStatus ApprovalStatus,
+    string Comment,
+    DateTime Date
+);
