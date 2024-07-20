4b950cda5346e242b8e26b1f0842d222c182fbb6
TauG <tauraigombera@gmail.com>
2024-07-20 13:27:08 +0200

    updated the ApiDbContext

--- Data/ApiDbContext.cs
+++ Data/ApiDbContext.cs
@@ -10,14 +10,23 @@ public class ApiDbContext : DbContext
     {   
     }
     public DbSet<Loan> Loans { get; set; }
+    public DbSet<ApprovalHistory> ApprovalHistories { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Loan>()
             .Property(l => l.LoanAmount)
             .HasColumnType("decimal(18,2)"); // Adjust precision and scale as per your requirements
 
-            modelBuilder.Entity<Loan>()
+        modelBuilder.Entity<Loan>()
             .Property(l => l.MonthlyDeductionAmount)
             .HasColumnType("decimal(18,2)"); // Adjust precision and scale as per your requirements
+
+        modelBuilder.Entity<Loan>()
+            .Property(l => l.LoanStatus)
+            .HasConversion<string>(); // Store enum as strings
+
+        modelBuilder.Entity<ApprovalHistory>()
+            .Property(ah => ah.ApprovalStatus)
+            .HasConversion<string>(); // Store enum as strings
     }
 }
