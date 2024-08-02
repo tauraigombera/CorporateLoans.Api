3d139e5139594daa261a68f9fe6c316d2a6712ac
TauG <tauraigombera@gmail.com>
2024-08-02 21:56:33 +0200

    changed controller name and repository name

---
+++ Controllers/LoanApplicatonsController.cs
@@ -0,0 +1,121 @@
+﻿using EmployeeLoans.Api.Dtos.ApprovalDtos;
+using EmployeeLoans.Api.Dtos.LoanDtos;
+using EmployeeLoans.Api.Enums;
+using EmployeeLoans.Api.Extensions;
+using EmployeeLoans.Api.Models;
+using EmployeeLoans.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeLoans.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LoanApplicationsController : ControllerBase
+{
+    private readonly ILoanApplicationRepository _loanApplicationRepository;
+
+    public LoanApplicationsController(ILoanApplicationRepository loanRepository)
+    {
+        _loanApplicationRepository = loanRepository;
+    }
+
+    //GET /loans
+    [HttpGet]
+    public async Task<IEnumerable<LoanApplicationDto>> GetLoans()
+    {
+        var loans = (await _loanApplicationRepository.GetLoansAsync())
+                .OrderBy(loan => loan.ApplicationDate)
+                .Select(loan => loan.AsDto());
+        return loans;
+    }
+
+    //GET /loans/{id}
+    [HttpGet("{id}")]
+    public async Task<ActionResult<LoanApplicationDto>> GetLoan(Guid id)
+    {
+        var loan = await _loanApplicationRepository.GetLoanAsync(id);
+
+        if (loan is null){
+            return NotFound();
+        }
+        return loan.AsDto();
+    }
+
+    //POST /loans
+    [HttpPost]
+    public async Task<ActionResult<LoanApplicationDto>> CreateLoans(CreateLoanApplicationDto createLoanDto)
+    {
+        LoanApplication loan = new(){
+            Id = Guid.NewGuid(),
+            LoanAmount = createLoanDto.LoanAmount,
+            LoanPurpose = createLoanDto.LoanPurpose,
+            MonthlyDeductionAmount = createLoanDto.MonthlyDeductionAmount,
+            LoanApplicationStatus = LoanStatus.Pending, //Default value
+            ApplicationDate = DateTime.Now
+        };
+
+        await _loanApplicationRepository.CreateLoanAsync(loan);
+        return CreatedAtAction(nameof(GetLoan), new{id = loan.Id}, loan.AsDto());
+    }
+
+    //PUT /loans/{id}
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateLoan(Guid id, UpdateLoanApplicationDto updateLoanDto)
+    {
+        LoanApplication? existingLoan = await _loanApplicationRepository.GetLoanAsync(id);
+
+        if (existingLoan is not null)
+        {
+            existingLoan.LoanAmount = updateLoanDto.LoanAmount;
+            existingLoan.LoanPurpose = updateLoanDto.LoanPurpose;
+            existingLoan.MonthlyDeductionAmount = updateLoanDto.MonthlyDeductionAmount;
+
+            await _loanApplicationRepository.UpdateLoanAsync(existingLoan);
+
+            return NoContent();
+        }
+        
+        return NotFound();
+    }
+
+
+ 
+    //POST /loans {loanId}/approve
+    [HttpPost("{loanId}/approve")]
+    public async Task<ActionResult<LoanApplicationDto>> ApproveLoan(Guid loanId, CreateApprovalHistoryDto createApprovalHistoryDto)
+    {
+        LoanApplication? existingLoan = await _loanApplicationRepository.GetLoanAsync(loanId);
+
+        if (existingLoan is not null){
+                ApprovalHistory approvalHistory = new(){
+                Id = Guid.NewGuid(),
+                LoanApplicationId = existingLoan.Id,
+                ApprovalOffice = createApprovalHistoryDto.ApprovalOffice,
+                Comment = createApprovalHistoryDto.Comment,
+                ApprovalDate = DateTime.Now
+            };
+            
+            await _loanApplicationRepository.ApproveLoanAsync(approvalHistory);
+            existingLoan.LoanApplicationStatus = LoanStatus.Approved;
+            await _loanApplicationRepository.UpdateLoanAsync(existingLoan);
+
+            return Ok(existingLoan);
+        }
+        
+        return NotFound();
+    }
+
+    //DELETE /organization/{id}
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteLoan(Guid id)
+    {
+        LoanApplication? loan = await _loanApplicationRepository.GetLoanAsync(id);
+        
+        if (loan is not null)
+        {
+           await _loanApplicationRepository.DeleteLoanAsync(id);
+        } 
+
+        return NoContent();
+    }
+}
