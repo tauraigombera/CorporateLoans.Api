70ff002d6c04c5ed61267e96dc901b7d54044631
TauG <tauraigombera@gmail.com>
2024-08-02 15:30:05 +0200

    renamed LoanExtension class to LoanApplicationExtention

---
+++ Extensions/LoanApplicationExtension.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeLoans.Api.Dtos.LoanDtos;
+using EmployeeLoans.Api.Models;
+
+namespace EmployeeLoans.Api.Extensions;
+
+public static class LoanExtension
+{
+    public static LoanApplicationDto AsDto(this LoanApplication loanApplication)
+    {
+        return new LoanApplicationDto
+        (
+            loanApplication.Id,
+            loanApplication.LoanAmount,
+            loanApplication.LoanPurpose,
+            loanApplication.ApplicationDate,
+            loanApplication.MonthlyDeductionAmount
+        );
+    }
+}
